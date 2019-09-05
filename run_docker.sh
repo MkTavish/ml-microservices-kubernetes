cf75e1bca1dc8e1a86deac3bc29c93a95c034310
root <root@ip-172-31-16-237.ec2.internal>
2019-09-05 15:40:17 +0000

    changes made to Dockerfile, make_prediction and run_docker

--- run_docker.sh
+++ run_docker.sh
@@ -5,8 +5,13 @@
 # Step 1:
 # Build image and add a descriptive tag
 
+docker build . -t mktavish/udacity-ml-microservice-api:v1 
+
 # Step 2: 
 # List docker images
 
+docker image ls -f label=name=udacity-ml-microservice-api
+
 # Step 3: 
 # Run flask app
+docker run -p 172.31.16.237:8080:80 udacity-ml-microservice-api:v1
