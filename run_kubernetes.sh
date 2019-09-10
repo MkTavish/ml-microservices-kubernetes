f78eebd6d616dd69d55dc2f57bb533d735c3b2f2
root <root@ip-172-31-87-45.ec2.internal>
2019-09-10 14:32:48 +0000

    configured to run docker with kubectl

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=mktavish/udacity-ml-microservice-api:v1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacity-ml-microservice-api --image=mktavish/udacity-ml-microservice-api:v1 --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+echo "sleeping for 10 seconds while waiting for pod to come up"
+sleep 10
+kubectl port-forward deployment/udacity-ml-microservice-api 8080:80
+kubectl logs --selector app=udacity-ml-microservice-api
 
